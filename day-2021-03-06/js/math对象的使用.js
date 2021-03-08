71946bb06cca98332091235fc09a0261ce4835df
xx025 <71559822+xx025@users.noreply.github.com>
2021-03-08 19:05:24 +0800

    math对象的使用

---
+++ day-2021-03-06/js/math对象的使用.js
@@ -0,0 +1,15 @@
+/*
+
+*   math对象的使用
+* */
+
+
+let a = -3.7;
+console.log(Math.abs(a));
+console.log(Math.round(a));
+for (let i = 0; i < 10; i++) {
+    console.log(Math.random());
+}
+
+
+console.log(Math.PI);
