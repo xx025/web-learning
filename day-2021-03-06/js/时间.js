311ddf737750b80c1951269d95ddfcf9b9426166
xx025 <71559822+xx025@users.noreply.github.com>
2021-03-08 19:05:35 +0800

    时间

---
+++ day-2021-03-06/js/时间.js
@@ -0,0 +1,13 @@
+/*          Date
+* */
+
+let now = new Date();
+console.log(now);
+console.log("年" + now.getFullYear());
+console.log("月" + now.getMonth());
+console.log("日" + now.getDay());
+console.log("星期" + now.getDay());//周天为0
+
+/*
+*       moment时间库
+* */
